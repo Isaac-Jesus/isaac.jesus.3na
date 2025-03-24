b6150381799b12fca2265a8891a54abbd882e5ed
Isaac-Jesus <isaacjesuscosta33@gmail.com>
2025-03-24 02:12:34 +0000

    codigo não rodava

---
+++ aula04/target/debug/deps/aula04-4fcceaaffaad9ea9.d
@@ -0,0 +1,5 @@
+/workspaces/isaac.jesus.3na/aula04/target/debug/deps/aula04-4fcceaaffaad9ea9: src/lib.rs
+
+/workspaces/isaac.jesus.3na/aula04/target/debug/deps/aula04-4fcceaaffaad9ea9.d: src/lib.rs
+
+src/lib.rs:
---
+++ aula04/target/debug/deps/aula04-62e5478e545cb469.d
@@ -0,0 +1,7 @@
+/workspaces/isaac.jesus.3na/aula04/target/debug/deps/libaula04-62e5478e545cb469.rmeta: src/lib.rs
+
+/workspaces/isaac.jesus.3na/aula04/target/debug/deps/libaula04-62e5478e545cb469.rlib: src/lib.rs
+
+/workspaces/isaac.jesus.3na/aula04/target/debug/deps/aula04-62e5478e545cb469.d: src/lib.rs
+
+src/lib.rs:
